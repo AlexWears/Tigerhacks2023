227136e9b95551851315b1e99c9ec0fd6ef110cd
houghdaniel <hough.daniel.s@gmail.com>
2023-11-03 21:33:15 -0500

    Added files for all characters

---
+++ benny.py
@@ -0,0 +1,19 @@
+import pygame
+
+from character import Character
+
+class Benny(Character):
+
+    def __init__(self, game):
+        super().__init__(game)
+
+        self.image = pygame.image.load("sprites/benny.bmp")
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = (self.settings.width/2, self.settings.height/2)
+        self.s_loc = 1
+    
+    def blit_benny(self):
+        self.screen.blit(self.image, self.rect)
+
+    def attack(self):
+        
--- character.py
+++ character.py
@@ -2,13 +2,15 @@ import pygame
 from settings import settings
 
 
-class character(pygame.sprite.Sprite):
+class Character(pygame.sprite.Sprite):
 
     def __init__(self, game):
         self.settings = game.settings
         self.screen = game.screen
         self.screen_rect = game.screen.get_rect()
         self.image = pygame.image.load(".")
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = (self.settings.width/2, self.settings.height/2)
         self.health = 0
         self.c_loc = 0
         self.s_loc = 0
@@ -18,6 +20,4 @@ class character(pygame.sprite.Sprite):
         self.screen.blit(self.image, self.rect)
 
     def update(self):
-
-
-    def attack(self):
+        self.blit_character()
