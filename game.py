b4c5fae034a35ffd8a5fcaa6523d92ccea143bc0
Alex Wears <alexanderwears02@gmail.com>
2023-11-04 20:50:20 -0500

    asdf

--- game.py
+++ game.py
@@ -80,6 +80,7 @@ class Game:
             self.scene.draw()
             for character in self.character_sprites.sprites():
                 character.draw()
+            self.sprites.draw()
 
             # Done after drawing everything to the screen
             pygame.display.flip()
